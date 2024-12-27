fcff074427237afe22376a47d118fbbf72d8cd39
TK Side A <apopulargamer@gmail.com>
2024-12-28 00:42:03 +0100

    add new

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,27 @@
 import React from 'react';
-import {Divider, Space} from "antd";
+import {Col, Divider, Row, Space} from "antd";
 import Button from "antd/es/button";
 import {useNavigate} from "react-router-dom";
 import {HOME_ROUTE, SIGN_IN_ROUTE} from "../../utils/const";
 import {LoginOutlined} from "@ant-design/icons";
+import Logo from '../../assets/Logo.svg'
 
 const Header = () => {
     const navigate = useNavigate();
 
 
     return (
-        <div style={{
-            display: 'flex',
-            justifyContent: "flex-end",
-            alignItems: "center",
-            gap: 14,
-            marginBottom: 24,
-            boxShadow: "0 0 4px 4px rgba(0, 0, 0, 0.15)",
-        }}>
-            <Button onClick={() => navigate(HOME_ROUTE)} type={"text"}>Мила фильмдерінің тізімі 🙈</Button>
-            {/*<Space direction="horizontal">
-                <Button onClick={() => navigate(SIGN_IN_ROUTE)} type="default"><LoginOutlined /></Button>
-            </Space>*/}
-            <div>
-                <Divider dashed/>
-            </div>
-        </div>
+        <Row style={{padding: 14, boxShadow: "2px 2px 4px rgba(0,0,0,0.25)"}} gutter={[16, 16]} justify="space-between">
+            <Col span={4}>
+                <Space style={{cursor: "pointer"}} onClick={() => navigate(HOME_ROUTE)}>
+                    <img style={{maxWidth: 35}} src={Logo} alt=""/>
+                    <span>Мила</span>
+                </Space>
+            </Col>
+            <Col>
+                <Button shape={"circle"}><LoginOutlined /></Button>
+            </Col>
+        </Row>
     );
 };
 
--- src/pages/Actor/SingleActor.tsx
+++ src/pages/Actor/SingleActor.tsx
@@ -38,56 +38,39 @@ const SingleActor = () => {
             .catch(err => console.error(err));
     }, [actor_id]);
 
-    useEffect(() => {
-        console.log(tv_credits);
-    }, [tv_credits]);
-
     if (!details || !images || !movie_credits || !tv_credits) {
         return null
     }
 
     return (
         <Row style={{padding: 14, maxWidth: 1400, margin: "0 auto"}} gutter={[16, 16]}>
-            <Col xs={8} xl={8}>
+            <Col xs={24} xl={8}>
                 <Image
                     width={"100%"}
                     src={`https://www.themoviedb.org/t/p/w500${details.profile_path}`}
                 />
             </Col>
-            <Col xs={16} xl={16}>
-                <Descriptions title={details.name}>
+            <Col xs={24} xl={16}>
+                <Descriptions size={"small"} title={details.name}>
                     <Descriptions.Item span={3} label="Birthday">{details.birthday}</Descriptions.Item>
-                    <Descriptions.Item span={3} label="place_of_birth">{details.place_of_birth}</Descriptions.Item>
-                    <Descriptions.Item span={3} label="also_known_as">
-                        {details.also_known_as.join(", ")}
-                    </Descriptions.Item>
+                    <Descriptions.Item span={3} label="Place of Birth">{details.place_of_birth}</Descriptions.Item>
                     <Descriptions.Item span={3}
-                                       label="known_for_department">{details.known_for_department}</Descriptions.Item>
+                                       label="Department">{details.known_for_department}</Descriptions.Item>
                     <Descriptions.Item span={3} label="popularity">{details.popularity}</Descriptions.Item>
                     <Descriptions.Item span={3} label="biography">{details.biography.slice(0, 250)}</Descriptions.Item>
                 </Descriptions>
-                <Space wrap={true} style={{marginTop: 24}}>
-                    {images.profiles.map((image) => {
-                        return (
-                            <Image
-                                width={85}
-                                src={`https://www.themoviedb.org/t/p/w500${image.file_path}`}
-                            />
-                        )
-                    })}
-                </Space>
             </Col>
-            <Col span={12} style={{position: "relative"}}>
-                <Title level={3}>Also movie with {details.name}</Title>
-                <Space wrap={true}>
-                    {movie_credits.cast.map((movie, index) => {
+            <Col span={24} style={{position: "relative"}}>
+                <Title level={4}>Movie with {details.name}</Title>
+                <Space style={{justifyContent: "space-between"}} wrap={true}>
+                    {movie_credits.cast.slice(0, 5).map((movie, index) => {
 
                         if (!movie.poster_path) {
                             return (
                                 <div style={{
-                                    minWidth: 150,
+                                    width: 65,
                                     backgroundColor: "rgba(0,0,0, 0.25)",
-                                    height: "225px",
+                                    height: "97.5px",
                                     display: "flex",
                                     justifyContent: "center",
                                     alignItems: "center"
@@ -99,7 +82,7 @@ const SingleActor = () => {
 
                         return (
                             <img
-                                style={{cursor: "pointer", maxWidth: 150}}
+                                style={{cursor: "pointer", maxWidth: 65}}
                                 onClick={() => navigate(`${SINGLE_MOVIE}?id=${movie.id}`)}
                                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                                 alt={movie.title}
@@ -108,17 +91,17 @@ const SingleActor = () => {
                     })}
                 </Space>
             </Col>
-            <Col span={12} style={{position: "relative"}}>
-                <Title level={3}>Also TV Shows with {details.name}</Title>
+            <Col span={24} style={{position: "relative"}}>
+                <Title level={4}>TV Shows with {details.name}</Title>
                 <Space wrap={true}>
-                    {tv_credits.cast.map((tv, index) => {
+                    {tv_credits.cast.slice(0, 5).map((tv, index) => {
 
                         if (!tv.poster_path) {
                             return (
                                 <div style={{
-                                    minWidth: 150,
+                                    width: 65,
                                     backgroundColor: "rgba(0,0,0, 0.25)",
-                                    height: "225px",
+                                    height: "97.5px",
                                     display: "flex",
                                     justifyContent: "center",
                                     alignItems: "center"
@@ -130,10 +113,22 @@ const SingleActor = () => {
 
                         return (
                             <img
-                                style={{cursor: "pointer", maxWidth: 150}}
+                                style={{cursor: "pointer", maxWidth: 65}}
                                 onClick={() => navigate(`${SINGLE_MOVIE}?id=${tv.id}`)}
                                 src={`https://image.tmdb.org/t/p/w500${tv.poster_path}`}
                                 alt={tv.name}
+                            />
+                        )
+                    })}
+                </Space>
+            </Col>
+            <Col span={24}>
+                <Space wrap={true} style={{marginTop: 24}}>
+                    {images.profiles.map((image) => {
+                        return (
+                            <Image
+                                width={85}
+                                src={`https://www.themoviedb.org/t/p/w500${image.file_path}`}
                             />
                         )
                     })}
